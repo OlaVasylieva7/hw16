84d6c48c5db5a9a8f119a39186d0346e916d69b6
OlaVasylieva7 <124078842+OlaVasylieva7@users.noreply.github.com>
2024-10-24 17:30:27 +0000

    Deploying to gh-pages from @ OlaVasylieva7/hw16@6cecaacf37e2d607c55c588e079d37ea8b1277d5 🚀

---
+++ index.1b5b2bb0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("input");document.getElementById("list"),document.getElementById("country");t.addEventListener("input",(function(t){t.preventDefault();var e=t.target.value.trim();console.log(e)}))}();
+//# sourceMappingURL=index.1b5b2bb0.js.map
