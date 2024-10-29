bc094ed28027349927860f33c7175e31e06f4639
OlaVasylieva7 <124078842+OlaVasylieva7@users.noreply.github.com>
2024-10-29 18:34:38 +0000

    Deploying to gh-pages from @ OlaVasylieva7/hw16@ddc71edd2279dcabb3137d934e1c8cd9083452d5 🚀

--- index.1b5b2bb0.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.getElementById("input");document.getElementById("list"),document.getElementById("country");t.addEventListener("input",(function(t){t.preventDefault();var e=t.target.value.trim();console.log(e)}))}();
-//# sourceMappingURL=index.1b5b2bb0.js.map
--- index.c87599f1.js
+++
@@ -1,2 +0,0 @@
-const t=document.getElementById("input");document.getElementById("list"),document.getElementById("country");t.addEventListener("input",(function(t){t.preventDefault();const e=t.target.value.trim();console.log(e)}));
-//# sourceMappingURL=index.c87599f1.js.map
