ddc71edd2279dcabb3137d934e1c8cd9083452d5
OlaVasylieva7 <olhavas74@gmail.com>
2024-10-29 19:34:10 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,53 @@
-// import debounce from "lodash.debounce";
-// import {alert, error as notifyError, info as notifyInfo} from '@pnotify/core';
-// import '@pnotify/core/dist/PNotify.css';
+import debounce from 'lodash.debounce';
+import { alert, error as notifyError, info as notifyInfo } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import fetchCountries from './js/fetchCountries';
 
+const input = document.getElementById('input');
+const list = document.getElementById('list');
+const country = document.getElementById('country');
 
-function fetchCountries(searchQuery) {
-    return fetch(
-      `https://restcountries.com/v3.1/name/${searchQuery}?fields=name,capital,population,flags,languages`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
-
-const input = document.getElementById("input");
-const list = document.getElementById("list");
-const country = document.getElementById("country");
-
-
-input.addEventListener('input', onInputChange)
+input.addEventListener('input', debounce(onInputChange, 500));
 
 function onInputChange(e) {
-e.preventDefault();
+  e.preventDefault();
 
-const inputValue = e.target.value.trim()
-console.log(inputValue);
+  const inputValue = e.target.value.trim();
+
+  if (inputValue === '') {
+    clearData();
+    return;
+  }
+
+  fetchCountries(inputValue)
+    .then(fetchResponse)
+    .catch(error => {
+      notifyError(`something went wrong ${error}`);
+    });
+}
+
+function clearData() {
+  list.innerHTML = '';
+  country.innerHTML = '';
+}
+
+function renderCountry(countryInfo) {
+  country.insertAdjacentHTML('beforeend', countryInfo);
+}
+
+function renderCountryList(countryList) {
+  list.insertAdjacentHTML('beforeend', countryList);
+}
+
+function fetchResponse(countries) {
+  if (countries.lenght > 1 && countries.lenght <= 10) {
+    const countryList = countries
+      .map(country => {
+        return `
+      <li><img class="list__img" src="${country.flags.svg}"><p>${country.name.official}</p></li>
+      `;
+      })
+      .join();
+    renderCountryList(countryList);
+  }
 }
